096365e1f96de0d0a735d7fa98d3f45bbfa19025
Sergi Perez <chechidatadev@gmail.com>
2025-07-01 11:58:32 +0200

    ADD: Pseudo-code

---
+++ Ex01_PrintComb/print_comb.py
@@ -0,0 +1,32 @@
+
+"""
+Escribir un programa en Python que muestre todas las combinaciones posibles de tres dígitos distintos en orden estrictamente creciente, utilizando listas y estructuras de control.
+
+# Requisitos funcionales:
+- El programa debe generar todas las combinaciones de tres dígitos (del 0 al 9) sin repetir dígitos y donde cada combinación cumpla que el primer dígito < segundo dígito < tercer dígito. Ejemplo válido: 027 Ejemplo inválido: 272, 321, 999.
+
+- El programa debe preguntar al usuario si desea que las combinaciones se muestren en:
+
+Orden normal (de menor a mayor): 012, 013, 014, ..., 789
+Orden invertido (de mayor a menor): 789, 788, ..., 012
+
+- El resultado debe mostrarse en pantalla, separado por comas, en una sola línea.
+
+# ---
+
+# Requisitos técnicos:
+No se permite usar itertools, set, sorted, sort(), ni funciones similares de ordenación o filtrado automático.
+Solo se debe utilizar listas, bucles for, y estructuras de control como if.
+El orden invertido debe implementarse manualmente, sin usar [::-1] ni .reverse().
+"""
+
+def print_comb(invert=False):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
